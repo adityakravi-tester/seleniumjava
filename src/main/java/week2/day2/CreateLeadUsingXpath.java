7739d2edeb4574a81d797bcedb773f45b06de093
uniphyuser <uniphyuser@gmail.com>
2021-04-18 22:29:50 +0530

    Added classword for week2 day2

---
+++ src/main/java/week2/day2/CreateLeadUsingXpath.java
@@ -0,0 +1,40 @@
+package week2.day2;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLeadUsingXpath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		driver.manage().window().maximize();
+		
+		driver.findElementByXPath("//input[@id='username']").sendKeys("Demosalesmanager");
+		
+		driver.findElementByXPath("//input[@id='password']").sendKeys("crmsfa");
+		
+		driver.findElementByXPath("//input[@class='decorativeSubmit']").click();
+				
+		driver.findElementByXPath("//a[contains(text(),'CRM/SFA')]").click();
+		
+		driver.findElementByXPath("//a[text()='Leads']").click();
+		
+		driver.findElementByXPath("//a[text()='Create Lead']").click();
+		
+		driver.findElementByXPath("//input[@name='companyName' and @id='createLeadForm_companyName']").sendKeys("Digital Software Inc");
+		
+		driver.findElementByXPath("//input[@id='createLeadForm_firstName']").sendKeys("Aditya");
+		
+		driver.findElementByXPath("//input[@id='createLeadForm_lastName']").sendKeys("Kumar");
+		
+		driver.findElementByXPath("//input[@name='submitButton']").click();
+	}
+
+}
